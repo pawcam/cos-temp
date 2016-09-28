519aa288a31d71acc14255cb586325dc56773750
Christopher Ingebrigtsen <alkalin3@gmail.com>
2016-09-28 15:57:29 -0500

    Or port (#3)
    * git subrepo commit lib
    
    subrepo:
      subdir:   "lib"
      merged:   "91410ab"
    upstream:
      origin:   "git@github.com:tastyworks/order-execution.git"
      branch:   "master"
      commit:   "7e4e4db"
    git-subrepo:
      version:  "0.3.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "1e79595"
    
    * Removed OR_PORT being a required env variable.

--- cos/main.cpp
+++ cos/main.cpp
@@ -43,11 +43,10 @@ int main(int argc, char *argv[]) {
     string mqPassword = getenv("MQ_PASSWORD");
     string mqVHost = getenv("MQ_VHOST");
     string mqExchangeName = getenv("MQ_EXCHANGE_NAME");
-    uint16_t orPort = lexical_cast<uint16_t>(getenv("OR_PORT"));
     string strORDefaultRoute = getenv("OR_DEFAULT_ROUTE");
     string mqQueueName = getenv("MQ_QUEUE_NAME");
 
-    OELAdapterType *pOelAdapter = new OELAdapterType(orPort, INPUT, strORDefaultRoute);
+    OELAdapterType *pOelAdapter = new OELAdapterType(INPUT, strORDefaultRoute);
     service = new service_t(instanceId, mqHost, mqPort, mqUsername, mqPassword, mqVHost, mqQueueName, mqExchangeName,
                             pOelAdapter);
     service->start();
