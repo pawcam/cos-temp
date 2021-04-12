5876e97c71984d0a245753d8282a4aa55c068a64
pawcam <campbell.pawlos@gmail.com>
2021-04-12 15:59:24 -0500

    Using direct exchange MQAdapter changes

--- cos/CancelOrderMessageHandler.h
+++ cos/CancelOrderMessageHandler.h
@@ -10,13 +10,14 @@ class OR2Adapter;
 
 class CancelOrderMessageHandler : public TW::MessageHandler {
 public:
-  CancelOrderMessageHandler(TW::OR2Adapter *pOR2Adapter, TW::SenderLocationReader *pSenderLocationReader);
+  CancelOrderMessageHandler(TW::OR2Adapter *pOR2Adapter, TW::SenderLocationReader *pSenderLocationReader, bool bDefaultRoute = false);
 
   virtual bool handleMessage(nlohmann::json& jMessage, std::string strTopic) override;
 
 private:
   TW::OR2Adapter *m_pOR2Adapter;
   TW::SenderLocationReader *m_pSenderLocationReader;
+  bool m_bDefaultRoute;
 };
 
 #endif //CANCELORDERMESSAGEHANDLER_H
