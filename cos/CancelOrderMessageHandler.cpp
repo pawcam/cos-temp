ed11c7de25f3c96bd0f845c27172ff349e0dcc98
Ray Manaloto <110417754+rmanaloto-tastytrade@users.noreply.github.com>
2023-09-14 13:42:15 -0500

    OE-1779 - Process "spread-symbol" from order-api  (#99)
    * link to latest order-execution 'origin/master'
    
    code cleanup
    
    * bracket initialize

--- cos/CancelOrderMessageHandler.cpp
+++ cos/CancelOrderMessageHandler.cpp
@@ -13,21 +13,17 @@ CancelOrderMessageHandler::CancelOrderMessageHandler(OR2Adapter *pOR2Adapter, TW
 
 //----------------------------------
 //----------------------------------
-bool CancelOrderMessageHandler::handleMessage(nlohmann::json& jMessage, std::string UNUSED(strTopic))
+bool CancelOrderMessageHandler::handleMessage(nlohmann::json& jMessage, std::string strTopic)
 {
   if (jMessage.empty() || !jMessage.is_object())
   {
     return false;
   }
 
-  return handleCancelOrderMessage(jMessage);
-}
-
-//----------------------------------
-//----------------------------------
-bool CancelOrderMessageHandler::handleCancelOrderMessage(const nlohmann::json& jMessage)
-{
-  SX_DEBUG("Attempting to handle a cancel message for order %s\n", jMessage.dump());
+  if(sx_logGetBit(sx_log::SX_LOG_DEBUG))
+  {
+    SX_DEBUG("[%s]Attempting to handle a cancel message for order %s\n", strTopic, jMessage.dump());
+  }
 
   TW::JsonOrderInterpreter orderWrapper = TW::JsonOrderInterpreter(jMessage, m_pSenderLocationReader);
   const std::string strDestination = m_bDefaultRoute ? m_pOR2Adapter->getDefaultRoute() : MQUtil::extractDestination(jMessage, m_pOR2Adapter->getDefaultRoute());
@@ -45,21 +41,40 @@ bool CancelOrderMessageHandler::handleCancelOrderMessage(const nlohmann::json& j
        (szMsg.h.uchType == sxORMsgWithType::MSG_COMPLEX_WRAPPER && szMsg.u.comp.nType == sxORMsgWithType::MSG_CANCEL_WITH_DETAILS))
     {
       const bool bSent = m_pOR2Adapter->sendCancel(szMsg, strDestination);
-      SX_DEBUG("%s Sent cancel request: "
-               "GON:%u "
-               "Identifier:%u "
-               "with sx message type %d "
-               "and szExchangeSymbol %s"
-               "\n"
-               , (bSent) ? "Successfully" : "Unsuccessfully"
-               , szMsg.u.cwd.nGlobalOrderNum
-               , szMsg.u.cwd.nowa.nIdentifier
-               , szMsg.h.uchType
-               , szMsg.u.cwd.nowa.no.szExchangeSymbol
-              );
+      if(sx_logGetBit(sx_log::SX_LOG_DEBUG))
+      {
+        uint32_t nGlobalOrderNum{std::numeric_limits<uint32_t>::max()};
+        uint32_t nIdentifier{std::numeric_limits<uint32_t>::max()};
+        const char* szOrderID{""};
+        const char* szExchangeSymbol{""};
+        auto pCancelWithDetailsMsg{sxORMsgWithType::getCancelWithDetailsMsgPtr(szMsg)};
+        if(pCancelWithDetailsMsg)
+        {
+          nGlobalOrderNum   = pCancelWithDetailsMsg->nGlobalOrderNum;
+          nIdentifier       = pCancelWithDetailsMsg->nowa.nIdentifier;
+          szOrderID         = pCancelWithDetailsMsg->szOrderID;
+          szExchangeSymbol  = pCancelWithDetailsMsg->nowa.no.szExchangeSymbol;
+        }
+        SX_DEBUG("%s Sent cancel request: "
+                 "[GON:%u]"
+                 "[Identifier:%u]"
+                 "[OrderID:%s]"
+                 "[message type:%d]"
+                 "[ExchangeSymbol:%s]"
+                 "\n"
+                 , (bSent) ? "Successfully" : "Unsuccessfully"
+                 , nGlobalOrderNum
+                 , nIdentifier
+                 , szOrderID
+                 , szMsg.h.uchType
+                 , szExchangeSymbol
+                );
+      }
       return bSent;
     }
-  } catch (const std::exception &e) {
+  }
+  catch (const std::exception &e)
+  {
     SX_ERROR("Failed to process cancel request - JSON Parser Error %s\n", e.what());
     return false;
   }
--- cos/CancelOrderMessageHandler.h
+++ cos/CancelOrderMessageHandler.h
@@ -13,14 +13,13 @@ class CancelOrderMessageHandler final : public TW::MessageHandler {
 public:
   CancelOrderMessageHandler(TW::OR2Adapter *pOR2Adapter, TW::SenderLocationReader *pSenderLocationReader, bool bDefaultRoute = false);
 
-  virtual bool handleMessage(nlohmann::json& jMessage, std::string strTopic) override;
+  bool handleMessage(nlohmann::json& jMessage, std::string strTopic) override;
 
 private:
   TW::OR2Adapter *m_pOR2Adapter;
   TW::SenderLocationReader *m_pSenderLocationReader;
   bool m_bDefaultRoute;
   MismatchedRootsMap m_rootMap;
-  bool handleCancelOrderMessage(const nlohmann::json& jMessage);
 };
 
 #endif //CANCELORDERMESSAGEHANDLER_H
